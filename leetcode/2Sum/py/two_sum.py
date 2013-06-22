f1675e42db31877983575a2076af1fa8be8e4706
Ilker <ilker.acar@gmail.com>
2013-06-22 16:55:38 -0700

    Add problem definition to 2sum py

--- leetcode/2Sum/py/two_sum.py
+++ leetcode/2Sum/py/two_sum.py
@@ -1,3 +1,16 @@
+"""
+Problem: Two Sum
+Given an array of integers, find two numbers such that they add up to a specific target number.
+
+The function twoSum should return indices of the two numbers such that they add up to the target,
+where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+
+You may assume that each input would have exactly one solution.
+
+Input: numbers={2, 7, 11, 15}, target=9
+Output: index1=1, index2=2
+
+"""
 # Two sum method that returns indices of two items that sum up to target
 def twoSum(numbers, target):
     if not numbers:
